bb0381d39cc79ec98d1f934ea2c8e320689552f6
DESKTOP-NVT8K0N\stanb <stanb22@yahoo.com>
2017-06-04 12:34:41 -0400

    Unreal possesing tank with using c++

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,7 +3,21 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
