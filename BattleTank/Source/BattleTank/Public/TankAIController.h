a1439c4edd0b8401367f1f7f5449cdaacc412620
DESKTOP-NVT8K0N\stanb <stanb22@yahoo.com>
2017-06-17 12:09:15 -0400

    Unreal adding tick to playerController

--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,9 +14,10 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 
+private:
 	virtual void BeginPlay() override;
 
 	ATank* GetControlledTank() const;
-	
-	
+
+	ATank * GetPlayerTank() const;
 };
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -18,5 +18,11 @@ public:
 	ATank* GetControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that the shot would hit
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
 	
 };
