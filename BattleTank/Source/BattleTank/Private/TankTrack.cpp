9cab08ec33049e1462ff4a9354da87f0ced2a068
DESKTOP-NVT8K0N\stanb <stanb22@yahoo.com>
2018-02-20 01:29:29 -0500

    Using BlueprintReadOnly (calling forward movement using a class)

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,10 +6,6 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-	//auto Time = GetWorld()->GetTimeSeconds();
-	auto Name = GetName();
-	UE_LOG(LogTemp, Warning, TEXT("%f: throttle: %f"), *Name, Throttle);
-
 	//TODO clamp actual throttle value so player cant over-drive
 	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
 	auto ForceLocation = GetComponentLocation();
